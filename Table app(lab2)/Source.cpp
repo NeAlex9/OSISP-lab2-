8508c6a085ca3c155883218d5e8af0e1fc2f2b0f
Алексей Невструев <leha.nevstruev.01@gmail.com>
2021-09-29 00:07:12 +0300

    Have to change input data to vector

--- Table app(lab2)/Source.cpp
+++ Table app(lab2)/Source.cpp
@@ -7,19 +7,28 @@
 using namespace std;
 
 #define PADDING 20
+#define REPAINT_TABLE 4
+#define CHANGE_COL 5
+#define CHANGE_ROW 6
+#define MAX_COL 10
+#define MAX_ROW 7
 
+int cols = 4, rows = 4;
+
+RECT rect;
 LRESULT CALLBACK WindowProc(HWND, UINT, WPARAM, LPARAM);
 void CreateTable(HDC, int, int, int, int, int);
-vector<vector<string>> stringMatrix = {
-	{"a", "dkkkkkkkkkkkkkyghjiuygyвывввывuвыаываыfghi7ygudasdasdasdsadd", "sdf", "ds"},
-	{"a", "d", "s", "d"},
-	{"", "", "", ""},
-	{"adыыыыыыыыыыыs", "dsыффффффффффффффффффfsd", "sdf", "ds"},
-	{"a", "d", "s", "d"},
-	{"a", "dkkkkkkkkkkkkkyghjiuygyвывввывuвыаываыfghi7ygudasdasdasdsadd", "sdf", "ds"},
-	{"a", "dkkkkkkkkkkkkkyghjiuygyвывввывuвыаываыfghi7ygudasdasdasdsadd", "sdf", "ds"}
+vector<string> stringMatrix = {
+	"a", "dkkkkkkkkkkkkkyghjiuygyвывввывuвыаываыfghi7ygudasdasdasdsadd", "sdf", "ds",
+	"a", "d", "s", "d",
+	"", "", "", "",
+	"adыыыыыыыыыыыs", "dsыффффффффффффффффффfsd", "sdf", "ds",
+	"a", "d", "s", "d",
+	"a", "dkkkkkkkkkkkkkyghjiuygyвывввывuвыаываыfghi7ygudasdasdasdsadd", "sdf", "ds",
+	"a", "dkkkkkkkkkkkkkyghjiuygyвывввывuвыаываыfghi7ygudasdasdasdsadd", "sdf", "ds"
 };
-static HBRUSH hBrush = CreateSolidBrush(RGB(0, 0, 0));
+
+static HBRUSH hBrush = CreateSolidBrush(RGB(32, 23, 233));
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) {
 	WNDCLASSEX wcex;
@@ -50,28 +59,18 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
 	);
 
 	HWND hWndEditRow = CreateWindow(
-		L"Edit", 
-		L"", 
-		WS_CHILD | WS_VISIBLE | WS_BORDER, 
-		70, 50, 120, 20, 
-		hWnd, NULL, NULL, NULL);
+		L"Edit",
+		L"",
+		WS_CHILD | WS_VISIBLE | WS_BORDER | ES_NUMBER,
+		70, 50, 120, 20,
+		hWnd, /*(HMENU)CHANGE_ROW*/NULL, NULL, NULL);
 
 	HWND hWndEditColumn = CreateWindow(
 		L"Edit",
 		L"",
-		WS_CHILD | WS_VISIBLE | WS_BORDER,
+		WS_CHILD | WS_VISIBLE | WS_BORDER | ES_NUMBER,
 		230, 50, 120, 20,
-		hWnd, NULL, NULL, NULL);
-
-	HWND hwndButton = CreateWindow(
-		L"BUTTON", 
-		L"OK",      
-		WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
-		400, 50, 60, 20,        
-		hWnd,     
-		NULL,       
-		(HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE),
-		NULL);      
+		hWnd, (HMENU)CHANGE_COL, NULL, NULL);
 
 	ShowWindow(hWnd, nCmdShow);
 	UpdateWindow(hWnd);
@@ -92,18 +91,71 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp)
 	int xStart = 0;
 	int yEnd = 0;
 	int tableWidth = 0;
+	int d;
 	switch (msg) {
 	case WM_PAINT:
 		hdc = BeginPaint(hwnd, &ps);
+		FillRect(hdc, &rect, (HBRUSH)(COLOR_WINDOW + 1));
 		TextOut(hdc, 90, 30, L"Row", lstrlen(L"Row"));
 		TextOut(hdc, 250, 30, L"Column", lstrlen(L"Column"));
-		RECT rec;
-		GetWindowRect(hwnd, &rec);
+		GetClientRect(hwnd, &rect);
 		xStart = 30;
 		yEnd = 30;
-		tableWidth = rec.right - rec.left - xStart - yEnd;
-		CreateTable(hdc, xStart, 100, tableWidth, 4, 4);
+		tableWidth = rect.right - rect.left - xStart - yEnd;
+		CreateTable(hdc, xStart, 100, tableWidth, cols, rows);
 		EndPaint(hwnd, &ps);
+		break;
+	case WM_COMMAND:
+		switch (LOWORD(wp))
+		{
+		case REPAINT_TABLE:
+			cols += 1;
+			InvalidateRect(hwnd, &rect, NULL);
+			break;
+		case CHANGE_COL:
+			switch (HIWORD(wp))
+			{
+			case EN_CHANGE:
+			{
+				wchar_t wc[255];
+				LPWSTR buff;
+				GetWindowText((HWND)lp, wc, 255);
+				std::wstring ws(wc);
+				std::string s = std::string(ws.begin(), ws.end());
+				if (s != "")
+					cols = std::stoi(s);
+				else
+					cols = 0;
+				if (cols < MAX_COL)
+				{
+					InvalidateRect(hwnd, &rect, NULL);
+				}
+			}
+			}
+			break;
+
+		case CHANGE_ROW:
+			switch (HIWORD(wp))
+			{
+			case EN_CHANGE:
+			{
+				wchar_t wc[255];
+				LPWSTR buff;
+				GetWindowText((HWND)lp, wc, 255);
+				std::wstring ws(wc);
+				std::string s = std::string(ws.begin(), ws.end());
+				if (s != "")
+					rows = std::stoi(s);
+				else
+					rows = 0;
+				if (rows < MAX_ROW)
+				{
+					InvalidateRect(hwnd, &rect, NULL);
+				}
+			}
+			}
+			break;
+		}
 		break;
 	case WM_DESTROY:
 		PostQuitMessage(0);
@@ -173,8 +225,8 @@ int GetRowHeight(HDC hdc, vector<string> row, int colWidth)
 
 HFONT GetFont(int height, int width)
 {
-	HFONT hFont = CreateFont(height, width, 0, 0, 2, 0, 
-false, FALSE, RUSSIAN_CHARSET, 20,
+	HFONT hFont = CreateFont(height, width, 0, 0, 2, 0,
+		false, FALSE, RUSSIAN_CHARSET, 20,
 		20, 20, 20,
 		TEXT("Times New Roman"));
 
@@ -184,10 +236,11 @@ false, FALSE, RUSSIAN_CHARSET, 20,
 void CreateTable(HDC hdc, int xStart, int yStart, int tableWidth, int rowsCount, int colCount)
 {
 	SelectObject(hdc, GetFont(20, 6));
+	int count = 0;
 	for (int i = 0; i < rowsCount; i++)
 	{
 		int xPos = xStart;
-		DrawLine(hdc, xPos, yStart,  xPos + tableWidth, yStart);
+		DrawLine(hdc, xPos, yStart, xPos + tableWidth, yStart);
 		int height = GetRowHeight(hdc, stringMatrix[i], tableWidth / colCount - PADDING);
 		for (int j = 0; j < colCount; j++)
 		{
